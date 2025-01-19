0aabd5e87ed33a6d990f08cd22c5fc1311be4022
Mohammed Shoaib <mohammedshoaib006@gmail.com>
2025-01-19 23:27:23 +0530

    first commit

---
+++ src/app/components/forgotpassword/forgotpassword.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgotpassword',
+  standalone: false,
+  
+  templateUrl: './forgotpassword.component.html',
+  styleUrl: './forgotpassword.component.scss'
+})
+export class ForgotpasswordComponent {
+
+}
